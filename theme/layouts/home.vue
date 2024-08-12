b414bd0725cda1f881da4a4d3da094b850969f1e
WRXinYue <3322543587@qq.com>
2024-08-12 12:37:44 +0800

    fix: layout nesting

--- theme/layouts/home.vue
+++ theme/layouts/home.vue
@@ -1,17 +1,15 @@
 <template>
-  <BaseLayout title="Home">
-    <BaseCard title="Home">
-      <div class="flex">
-        <div class="p-6">
-          <h1>Hi there! 👋</h1>
-          <p>This is the Frosti theme</p>
-          <p>A clean, elegant, and fast static blog template</p>
-        </div>
+  <BaseCard title="Home">
+    <div class="flex">
+      <div class="p-6">
+        <h1>Hi there! 👋</h1>
+        <p>This is the Frosti theme</p>
+        <p>A clean, elegant, and fast static blog template</p>
       </div>
-    </BaseCard>
+    </div>
+  </BaseCard>
 
-    <BaseCard title="Home" image="/index.webp">
-      <RouterView />
-    </BaseCard>
-  </BaseLayout>
+  <BaseCard title="Home" image="/index.webp">
+    <RouterView />
+  </BaseCard>
 </template>
